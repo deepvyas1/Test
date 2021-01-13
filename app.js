a1dfcad22c8704b69a4389da10ba35c9a089c2a0
deepvyas1 <deep.vyas@krayonnz.com>
2021-01-13 15:39:38 +0530

    updates

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
     res.status(200).send({key: "Its a demo response"});
 });
 
-app.listen(3000, () => {
+app.listen(8001, () => {
     console.log("Started Listening on port 3000...");
 })
